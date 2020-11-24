f161a09bf6f9847f0a46d0568aa448ecb94f7955
Justin Anderson <jander@microsoft.com>
2020-11-24 13:59:06 -0800

    wip

---
+++ src/Microsoft.Diagnostics.Monitoring.EventPipe/Triggers/GCInducedTriggerRule.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Diagnostics.Tracing;
+using Microsoft.Diagnostics.Tracing.Parsers.Clr;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.Diagnostics.Monitoring.EventPipe.Triggers
+{
+    internal class GCTriggerRule : ITriggerRule
+    {
+        private ITriggerRuleContext _context;
+        private State _state;
+
+        public async Task InitializeAsync(ITriggerRuleContext context, CancellationToken token)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+
+            await context.SetProvidersAsync(null, token);
+
+            _state = State.MonitorInducedGCs;
+        }
+
+        public void SetupCallbacks(TraceEventSource eventSource)
+        {
+            if (null == eventSource)
+            {
+                throw new ArgumentNullException(nameof(eventSource));
+            }
+
+            switch (_state)
+            {
+                case State.MonitorInducedGCs:
+                    eventSource.Clr.GCTriggered += Clr_GCTriggered;
+                    break;
+            }
+        }
+
+        private void Clr_GCTriggered(GCTriggeredTraceData obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        private enum State
+        {
+            MonitorInducedGCs
+        }
+    }
+}
---
+++ src/Microsoft.Diagnostics.Monitoring.EventPipe/Triggers/ITriggerRule.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Diagnostics.NETCore.Client;
+using Microsoft.Diagnostics.Tracing;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.Diagnostics.Monitoring.EventPipe.Triggers
+{
+    internal interface ITriggerRule
+    {
+        Task InitializeAsync(ITriggerRuleContext context, CancellationToken token);
+
+        void SetupCallbacks(TraceEventSource eventSource);
+    }
+
+    internal interface ITriggerRuleContext
+    {
+        Task SetProvidersAsync(IEnumerable<EventPipeProvider> providers, CancellationToken token);
+
+        Task EgressEventsAsync(string providerName, CancellationToken token);
+    }
+}
